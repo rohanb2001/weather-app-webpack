cab6d72f55efed785670eb04979b5a606c79ebc6
Ayan Banerjee <ayanbanerjee102@gmail.com>
2023-01-17 21:07:25 +0530

    Getting response from the fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./css/style.css";
+import clear from "./assets/clear.svg";
+import cloud from "./assets/cloud.svg";
+import haze from "./assets/haze.svg";
+import rain from "./assets/rain.svg";
+import snow from "./assets/snow.svg";
+import storm from "./assets/storm.svg";
+
 const wrapper = document.querySelector(".wrapper");
 const inputPart = document.querySelector("input-part");
 const infoTxt = document.querySelector(".info-txt");
@@ -21,7 +28,7 @@ function getInputValues(e) {
 
 function getGeoLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onSuccess);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
   } else {
     alert("Your browser not support geolocation api");
   }
@@ -29,7 +36,14 @@ function getGeoLocation() {
 
 function onSuccess(position) {
   const { latitude, longitude } = position.coords;
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
   console.log(latitude, longitude);
+  fetchData();
+}
+
+function onError(error) {
+  infoTxt.innerHTML = error.message;
+  infoTxt.classList.add("error");
 }
 
 function requestApi(city) {
@@ -42,11 +56,27 @@ function fetchData() {
   infoTxt.classList.add("pending");
   fetch(api)
     .then((res) => res.json())
-    .then((result) => console.log(result))
+    .then((result) => weatherDetails(result))
     .catch(() => {
       infoTxt.innerHTML = "Something went wrong";
       infoTxt.classList.replace("pending", "error");
     });
+}
+
+function weatherDetails(info) {
+  if (info.cod === "404") {
+    infoTxt.classList.replace("pending", "error");
+    infoTxt.innerHTML = `${inputField.value} isn't a vlid city name`;
+  } else {
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { temp, feels_like, humidity } = info.main;
+
+    if (id === 800) {
+      wIcon.src = `assets/${clear}`;
+    }
+  }
 }
 
 // Add Event listeners
